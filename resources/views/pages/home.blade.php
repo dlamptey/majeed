bc50cc0fda4e477e3097963e7d343cd947136e2f
Derrick Lamptey <derrick.lamptey@gmail.com>
2017-10-15 15:28:39 +0100

    First commit to repo

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,296 @@
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://opengraph.org/schema/"><head><style class="vjs-styles-defaults">
+      .video-js {
+        width: 300px;
+        height: 150px;
+      }
+
+      .vjs-fluid {
+        padding-top: 56.25%
+      }
+    </style>
+<title>Beyoncé</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="URL" content="{{url("/")}}">
+<link rel="shortcut icon" href="{{url("/resources/assets/")}}/images/favicon.png">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="https://www.beyonce.com/xmlrpc.php">
+<meta name="copyright" content="2017">
+<link rel="dns-prefetch" href="//s.w.org">
+<link rel="stylesheet" id="app-css-css" href="{{url("/resources/assets/")}}/stylesheets/styles.css?ver=1493655339" type="text/css" media="all">
+<script type="text/javascript" async="" src="/_Incapsula_Resource?SWJIYLWA=719d34d31c8e3a6e6fffd425f7e032f3&amp;ns=1&amp;cb=1745167634"></script><script async="" src="//www.google-analytics.com/analytics.js"></script><script type="text/javascript" async="" src="https://cdn.heapanalytics.com/js/heap-3826276139.js"></script><script>
+	window._se_plugin_version = '8.1.9';
+</script>
+<meta property="og:site_name" content="Beyonce">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Beyonce">
+<meta property="og:url" content="https://www.beyonce.com">
+<meta property="og:description" content="The Official Website of Majeed Buari">
+<meta property="og:image" content="{{url("/resources/assets/")}}/images/og-img.jpg">
+<meta property="fb:app_id" content="863803020305145">
+<meta property="fb:admins" content="537141755">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@beyonce">
+<meta name="twitter:title" content="Beyonce">
+<meta name="twitter:description" content="The Official Website of Beyoncé">
+<meta name="twitter:creator" content="@beyonce">
+<meta name="twitter:image" content="{{url("/resources/assets/")}}/images/og-img.jpg">
+<script>
+    // It's 2017 but we still have to force Safari to reload!
+    if (/Safari/.test(window.navigator.userAgent) && !/Chrome/.test(window.navigator.userAgent)) {
+        window.onpageshow = function(event) {
+            if (event.persisted) {
+                window.location.reload()
+            }
+        };
+    }
+</script>
+<script>
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+      heap.load("3826276139");
+</script>
+<meta class="foundation-mq"></head>
+<body class="home blog" cz-shortcut-listen="true">
+<div id="loading-mask"></div>
+<div id="header-wrapper">
+<header id="main-header">
+<button id="nav-trigger-mobile">
+    <span></span>
+    <span></span>
+    <span></span>
+</button>
+<a id="main-brand" href="/">
+<img src="{{url("/resources/assets/")}}/images/logo.jpg" alt="Majeed Buari" style="width: 50px;height: 70px;">
+
+</a>
+<div class="top-right-container">
+<div class="boxset-container">
+<a href="/beygoodhouston/">
+<div>
+<h4>Majeed Buari</h4>
+<h4>Foundation</h4>
+</div>
+<div>
+<img src="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.8.0/flags/1x1/gh.svg" style="width: 50px; height: 30px;" alt="Ghana Flag">
+</div>
+</a>
+</div>
+</div>
+</header>
+</div>
+<div id="nav-desktop">
+<button id="nav-trigger"><span></span><span></span><span></span></button>
+<nav id="main-nav-desktop">
+<ul>
+<li class="nav-desktop-feature-order-link">
+<a href="/beygoodhouston/" target="_blank">
+Support Houston Relief Efforts </a>
+</li>
+<li>
+<a class="has-children" href="/album/">Music</a>
+<ul class="sub-nav">
+<li><a href="/album/">Albums</a></li>
+<li><a href="/track/">Songs</a></li>
+<li><a href="/video/">Videos</a></li>
+<li><a href="/dvd/">DVDs</a></li>
+</ul>
+</li>
+<li><a href="/tour/">Tour</a></li>
+<li><a href="/beygood/">#BeyGood</a></li>
+<li class="external"><a href="http://www.ivypark.com/" target="_blank">Ivy Park</a></li>
+<li>
+<a class="has-children" href="/vault/">Vault</a>
+<ul class="sub-nav">
+<li><a href="/vault/">All</a></li>
+<li><a href="/vault?type=editorial">Editorial</a></li>
+<li><a href="/vault?type=my-life">My Life</a></li>
+<li><a href="/vault?type=performance">Performance</a></li>
+</ul>
+</li>
+<li>
+<a class="has-children external" href="http://shop.beyonce.com/" target="_blank">Shop</a>
+<ul class="sub-nav">
+<li class="external"><a href="http://shop.beyonce.com/" target="_blank">Merch</a></li>
+<li class="external"><a href="http://www.beyonceparfums.com/" target="_blank">Fragrance</a></li>
+<li>
+<a href="/beygoodhouston/" target="_blank">
+<span>Support Houston</span><br>
+<span>Relief Efforts</span>
+</a>
+</li>
+</ul>
+</li>
+<li>
+<a href="/signup/">Join</a>
+</li>
+</ul>
+</nav>
+<div id="nav-utilities-desktop">
+<a id="secondary-brand-desktop" class="retract-nav" href="/"><img src="{{url("/resources/assets")}}/images/logo-new-white.svg" alt="Beyonce"></a>
+<div id="search-wrapper-desktop">
+<form id="global-search" method="post">
+<input value="">
+<button type="submit" class="icon-search"><img src="{{url("/resources/assets")}}/images/icon-search.svg" alt="Search"></button>
+</form>
+</div>
+</div>
+<div id="addtl-links-desktop">
+<ul id="legal-desktop">
+<li><a class="retract-nav" href="/follow/">Follow</a></li>
+<li><a class="retract-nav" href="/terms-conditions/">Terms</a></li>
+<li><a class="retract-nav" href="/privacy-policy/">Privacy</a></li>
+</ul>
+</div>
+</div>
+<div id="main">
+<div class="boxset-container-mobile sticky stuck">
+<a href="/beygoodhouston/">
+<div>
+<h4>Support Houston</h4>
+<h4>Relief Efforts</h4>
+</div>
+<div>
+<img src="/wp-content/themes/beyonce_2014/assets/images/texas-flag_01_360.png" alt="Lemonade Boxset">
+</div>
+</a>
+</div>
+<section id="home" class="section-container">
+<div id="grid-home" class="grid">
+<div class="grid-sizer"></div>
+<article class="grid-item grid-item-three" data-post-id="73221" data-post-type="post" data-order-number="11351">
+<a class="view-details" href="/sweet-sadie-13/" data-post-type="post" data-post-id="73221">
+<div class="grid-item-animation-overlay "></div>
+<div class="grid-item-overlay ">
+<div class="grid-item-overlay-inner">
+<h2>
+<span class="title">SWEET SADIE</span>
+</h2>
+</div>
+</div>
+<img src="{{url("/resources/assets/images/2017-10-12-PHOTO-00000049.jpg")}}" alt="SWEET SADIE"> </a>
+</article>
+<article class="grid-item grid-item-three" data-post-id="73225" data-post-type="post" data-order-number="11350">
+<a class="view-details" href="/sweet-sadie-12/" data-post-type="post" data-post-id="73225">
+<div class="grid-item-animation-overlay "></div>
+<div class="grid-item-overlay ">
+<div class="grid-item-overlay-inner">
+<h2>
+<span class="title">SWEET SADIE</span>
+</h2>
+</div>
+</div>
+<img src="{{url("/resources/assets/images/2017-10-12-PHOTO-00000050.jpg")}}" alt="SWEET SADIE"> </a>
+</article>
+<article class="grid-item grid-item-three" data-post-id="73229" data-post-type="post" data-order-number="11349">
+<a class="view-details" href="/sweet-sadie-11/" data-post-type="post" data-post-id="73229">
+<div class="grid-item-animation-overlay "></div>
+<div class="grid-item-overlay ">
+<div class="grid-item-overlay-inner">
+<h2>
+<span class="title">SWEET SADIE</span>
+</h2>
+</div>
+</div>
+<img src="{{url("/resources/assets/images/2017-10-12-PHOTO-00000051.jpg")}}" alt="SWEET SADIE"> </a>
+</article>
+<article class="grid-item grid-item-three" data-post-id="73234" data-post-type="post" data-order-number="11348">
+<a class="view-details" href="/sweet-sadie-10/" data-post-type="post" data-post-id="73234">
+<div class="grid-item-animation-overlay "></div>
+<div class="grid-item-overlay ">
+<div class="grid-item-overlay-inner">
+<h2>
+<span class="title">SWEET SADIE</span>
+</h2>
+</div>
+</div>
+<img src="{{url("/resources/assets/images/2017-10-12-PHOTO-00000053.jpg")}}" alt="SWEET SADIE"> </a>
+</article>
+
+<div style="margin: 0px; padding: 0px; border: none;">
+    <div style="margin: 0px; padding: 0px; border: none;"></div>
+    <div data-infinity-pageid="0" style="margin: 0px; padding: 0px; border: none;"></div>
+</div>
+
+<div style="margin: 0px; padding: 0px; border: none;"><div style="margin: 0px; padding: 0px; border: none;"></div><div data-infinity-pageid="1" style="margin: 0px; padding: 0px; border: none;"></div></div></div>
+<div id="grid-home-pagination">
+<a class="next" href="/home/page/2/?ajaxpagedcall=true">Next</a>
+</div>
+</section>
+<a id="back-to-top" href="#"><img src="/wp-content/themes/beyonce_2014/assets/images/icon-arrow-up.svg" alt="Back To Top"></a>
+</div>
+<div id="zoom-overlay"></div>
+<div id="zoom-lb">
+<button id="image-zoom-close"><img src="/wp-content/themes/beyonce_2014/assets/images/icon-close.svg" alt="Close"></button>
+<img class="expanded-img" src="" alt="">
+</div><div id="nav-mobile" class="slide-down">
+<nav id="main-nav-mobile">
+<ul>
+<li>
+<a href="#">
+Support Houston Relief Efforts </a>
+</li>
+<li><a href="#">Music</a></li>
+<li><a href="#">Tour</a></li>
+<li><a href="#">#MajeedGood</a></li>
+<li class="external"><a href="#" target="_blank">Majeed Park</a></li>
+<li><a href="#">Vault</a></li>
+<li class="external"><a href="http://shop.beyonce.com/" target="_blank">Shop</a></li>
+<li><a href="/signup/">Join</a></li>
+</ul>
+</nav>
+<div id="nav-utilities-mobile">
+<div id="search-wrapper-mobile">
+<form id="global-search-mobile" method="post">
+<input value="">
+<button type="submit" class="icon-search"><img src="/wp-content/themes/beyonce_2014/assets/images/icon-search.svg" alt="Search"></button>
+</form>
+</div>
+</div>
+<div id="addtl-links-mobile">
+<ul id="legal-mobile">
+<li><a class="retract-nav-mobile" href="/follow/">Follow</a></li>
+<li><a class="retract-nav-mobile" href="/terms-conditions/">Terms</a></li>
+<li><a class="retract-nav-mobile" href="/privacy-policy/">Privacy</a></li>
+</ul>
+</div>
+</div>
+<script src="{{url("/resources/assets/")}}/scripts/app.js?ver=1493655345"></script>
+
+<script>
+var isReadyForTheJelly = function($) {
+    window.ajaxUrl = '/wp-content/ajax.php';
+    window.templateUrl = '/wp-content/themes/beyonce_2014';
+    var
+      opts = {
+        ajaxUrl : '/wp-content/ajax.php',
+        templateUrl : '/wp-content/themes/beyonce_2014',
+        ajaxContainer : '#main',
+        splashView : 0
+      };
+    // Create the beyonce app as the global var "beyonce"
+    window.beyonce = new App(opts);
+};
+// Check if jQuery is ready for the jelly?
+jQuery(isReadyForTheJelly());
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-30239633-1', 'beyonce.com');
+  ga('send', 'pageview');
+</script>
+<script type="text/javascript">
+//<![CDATA[
+(function() {
+var _analytics_scr = document.createElement('script');
+_analytics_scr.type = 'text/javascript'; _analytics_scr.async = true; _analytics_scr.src = '/_Incapsula_Resource?SWJIYLWA=719d34d31c8e3a6e6fffd425f7e032f3&ns=1&cb=1745167634';
+var _analytics_elem = document.getElementsByTagName('script')[0]; _analytics_elem.parentNode.insertBefore(_analytics_scr, _analytics_elem);
+})();
+// ]]>
+</script>
+</body>
+</html>
